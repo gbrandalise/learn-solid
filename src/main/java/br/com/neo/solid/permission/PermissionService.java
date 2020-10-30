cc52d036170ffa1e447c6bfafc54dfa5f7eaa2aa
giovannybrandalise <giovannybrandalise@brq.com>
2020-10-30 15:52:36 -0300

    ajuste profile

--- src/main/java/br/com/neo/solid/permission/PermissionService.java
+++ src/main/java/br/com/neo/solid/permission/PermissionService.java
@@ -1,11 +1,13 @@
 package br.com.neo.solid.permission;
 
+import br.com.neo.solid.profile.Profile;
 import br.com.neo.solid.user.User;
 
 public class PermissionService {
 
     public void setDefaultPermissions(User user) {
-        user.setPermissions(user.getProfile().getDefaultPermissions());
+        Profile profile = user.getProfile();
+        user.setPermissions(profile.getDefaultPermissions());
     }
     
 }
