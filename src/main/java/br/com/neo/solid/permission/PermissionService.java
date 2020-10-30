73790ab94d87d34830c669ffff8503f668b8fb33
giovannybrandalise <giovannybrandalise@brq.com>
2020-10-30 15:52:36 -0300

    ajuste profile

--- src/main/java/br/com/neo/solid/permission/PermissionService.java
+++ src/main/java/br/com/neo/solid/permission/PermissionService.java
@@ -1,34 +1,14 @@
 package br.com.neo.solid.permission;
 
-import java.util.Arrays;
-
+import br.com.neo.solid.profile.Profile;
 import br.com.neo.solid.user.User;
 import br.com.neo.solid.user.User.Profile;
 
 public class PermissionService {
 
     public void setDefaultPermissions(User user) {
-        if (user.getProfile() == Profile.ADMIN) {
-            user.setPermissions(Arrays.asList(
-                Permission.PUBLISH, 
-                Permission.CREATE, 
-                Permission.UPDATE, 
-                Permission.DELETE, 
-                Permission.LIST
-            ));
-        } else if (user.getProfile() == Profile.PUBLISHER) {
-            user.setPermissions(Arrays.asList(
-                Permission.PUBLISH,
-                Permission.DELETE,
-                Permission.LIST
-            ));            
-        } else {
-            user.setPermissions(Arrays.asList(
-                Permission.CREATE,
-                Permission.LIST,
-                Permission.UPDATE
-            ));
-        }
+        Profile profile = user.getProfile();
+        user.setPermissions(profile.getDefaultPermissions());
     }
     
 }
