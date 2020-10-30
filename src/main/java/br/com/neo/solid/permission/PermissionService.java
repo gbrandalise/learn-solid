006daf3ea76b566525fdc02efb734dfc827329f5
giovannybrandalise <giovannybrandalise@brq.com>
2020-10-30 15:36:53 -0300

    correcao permission

--- src/main/java/br/com/neo/solid/permission/PermissionService.java
+++ src/main/java/br/com/neo/solid/permission/PermissionService.java
@@ -1,4 +1,4 @@
-package br.com.neo.solid.profile;
+package br.com.neo.solid.permission;
 
 import java.util.Arrays;
 
@@ -8,22 +8,22 @@ import br.com.neo.solid.user.User.Profile;
 public class PermissionService {
 
     public void setDefaultPermissions(User user) {
-        if (user.getUserType() == Profile.ADMIN) {
-            user.setProfiles(Arrays.asList(
+        if (user.getProfile() == Profile.ADMIN) {
+            user.setPermissions(Arrays.asList(
                 Permission.PUBLISH, 
                 Permission.CREATE, 
                 Permission.UPDATE, 
                 Permission.DELETE, 
                 Permission.LIST
             ));
-        } else if (user.getUserType() == Profile.PUBLISHER) {
-            user.setProfiles(Arrays.asList(
+        } else if (user.getProfile() == Profile.PUBLISHER) {
+            user.setPermissions(Arrays.asList(
                 Permission.PUBLISH,
                 Permission.DELETE,
                 Permission.LIST
             ));            
         } else {
-            user.setProfiles(Arrays.asList(
+            user.setPermissions(Arrays.asList(
                 Permission.CREATE,
                 Permission.LIST,
                 Permission.UPDATE
